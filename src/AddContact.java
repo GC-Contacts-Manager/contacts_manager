8e824f63c26c117550d703117896588425261042
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-24 16:27:10 -0500

    updating add contact method

--- src/AddContact.java
+++ src/AddContact.java
@@ -15,41 +15,6 @@ public class AddContact {
     public AddContact() {
     }
 
-
-
-    public void AddNewContact(String name) {
-        String directory = "contacts"; // goes back to the root of the project
-        String filename = "contacts.txt"; // reason for strings is it's just easier to work with atm
-
-        Path dataDirectory = Paths.get(directory);
-        Path dataFile = Paths.get(directory,filename); // atm, this file doesn't actually exist
-
-        List<String> contactsList = Arrays.asList("John Marston || 123456789", "Arthur Morgan || 2103314814", "Dutch Van Der Linde || 2103260611");
-
-//        Scanner input = new Scanner(System.in);
-//        String contactName = input.getString("Enter the contacts name and number: ");
-
-        try {
-            Files.write(dataFile, contactsList);
-            List<String> newContactList = Arrays.asList(name);
-            Files.write(
-                    dataFile,
-                    newContactList,
-                    StandardOpenOption.APPEND);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        try {
-            List<String> contactListFromFile = Files.readAllLines(dataFile);
-            for (int i = 1; i <= contactListFromFile.size(); i+= 1){ // if you want it numbered !
-                System.out.println(i + ": " + contactListFromFile.get(i - 1)); // .get is getting value from a list (index)
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-
-    }
-
     public String getName() {
         return name;
     }
@@ -68,4 +33,50 @@ public class AddContact {
     }
 
 
+    public void AddNewContact(String name) {
+        String directory = "contacts"; // goes back to the root of the project
+        String filename = "contacts.txt"; // reason for strings is it's just easier to work with atm
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename); // atm, this file doesn't actually exist
+
+        List<String> contactsList = Arrays.asList("John Marston || 123456789", "Arthur Morgan || 2103314814", "Dutch Van Der Linde || 2103260611");
+
+        Scanner input = new Scanner(System.in);
+        // String contactName = input.getString("Enter the contacts name and number: ");
+        String addedContact = input.nextLine();
+        boolean isAdded = false;
+
+        try {
+            List<String> lines = Files.readAllLines(dataFile);
+
+            for (String line : lines) {
+                if (line.equals(addedContact)) {
+                    isAdded = true;
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        try {
+            Files.write(dataFile, contactsList);
+            List<String> newContactList = Arrays.asList(name);
+            Files.write(
+                    dataFile,
+                    newContactList,
+                    StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            List<String> contactListFromFile = Files.readAllLines(dataFile);
+            for (int i = 1; i <= contactListFromFile.size(); i += 1) { // if you want it numbered !
+                System.out.println(i + ": " + contactListFromFile.get(i - 1)); // .get is getting value from a list (index)
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
